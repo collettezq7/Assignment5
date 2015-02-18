4f25c376d4d2272c38a952edbebc4c7a8e6badc3
collettezq7 <colleensmith@csmith.local>
2015-02-17 21:26:29 -0700

    Final Changes - Silly Themeroller Colors

--- leavePage.js
+++ leavePage.js
@@ -8,13 +8,31 @@ $(document).ready(function()	{
 
 
 
-	function promptUser(){
-		var answer = confirm("Your session is expiring soon. Do you want to stay on this page, buddy?");
-	if (answer == true){
+	function promptUser() {
+	var popup = document.querySelector("div#popup");
+	var hidden = true;
+
+	if(hidden) {
 		setTimeout(promptUser, 10000);
-		}
-	else{
-		window.location.href="http://www.google.com";}
-		}
-		setTimeout(promptUser, 10000);
+		popup.classList.remove("hidden");
+		hidden = true;
+
+		var stay = document.querySelector("button#stay");
+		var exit = document.querySelector("button#exit");
+
+		stay.addEventListener('click', function (evt) {
+			console.log('clicked');
+			popup.classList.add("hidden");
+			hidden = true;
+			setTimeout(promptUser, 10000);
+		})
+
+		exit.addEventListener('click', function (evt) {
+			console.log('clicked');
+			window.location.href="http://www.google.com";
+		})
+	}
+}
+
+setTimeout(promptUser, 10000);
 });	
